f3f6fb07eed1c722c397ace681cbfb186f925d6a
thanhtrucN12 <thtrucnguyen91@gmail.com>
2021-02-25 03:14:38 -0800

    adjust hello world code
    adjust hello world code

--- Helloworld.py
+++ Helloworld.py
@@ -1,5 +1,12 @@
-#
-# This is hello world
-#
+""""
+This is hello world program in python
+"""
+import sys
 
+#print the version of python are running, it should be 3.x
+print()
+print("Version is: ", sys.version)
+print()
+
+#print hello world
 print("Hello world")
